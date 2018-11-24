1e7ed67c8c349a3c67a1cc5d3f1edbf5e31755f8
Harp Athwal <ha227@cornell.edu>
2018-11-23 19:57:03 -0500

    First commit includes example level 1.

---
+++ MathFeelLevel1/Assets/Script/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+    public Vector3 newPosition,originalPosition;
+    public Transform mainCamera;
+    public Text startText;
+    public void selfDestruct() { Destroy(this); }
+    public void DestroyText() { Destroy(startText); }
+
+    // Use this for initialization
+    void Start () {
+        mainCamera = transform.parent.transform;
+        originalPosition = mainCamera.position;
+        newPosition = originalPosition;
+        newPosition.x = -47;
+        mainCamera.position = newPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(originalPosition, newPosition) > 0.1f) {
+            if (System.Math.Floor(Vector3.Distance(originalPosition, newPosition))>15)
+            {
+                newPosition.x += 0.03f;
+                mainCamera.position = newPosition;
+            }
+            else if (Vector3.Distance(originalPosition, newPosition) > 0.1f)
+            {
+                newPosition.x += 0.08f;
+                mainCamera.position = newPosition;
+            }
+        }
+        else
+        {
+            startText.text = "Try your best to visualize in your head the function y = (x-1)*(x-2) and please place 3 points that would all be on this curve. Each gridline represents 1 unit.";
+            Invoke("DestroyText", 3.0f);
+            Invoke("selfDestruct", 3.1f);
+            
+
+        }
+    }
+}
---
+++ MathFeelLevel1/Assets/Script/PlotPoint.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+using Vector = MathNet.Numerics.LinearAlgebra.Vector<double>;
+
+
+
+
+public class PlotPoint : MonoBehaviour
+{
+
+		void s() {
+			Matrix A = Matrix.Build.DenseOfArray(new double[,] {
+				{1,0,0},
+				{0,2,0},
+				{0,0,3}});
+
+			Vector b = Vector.Build.Dense(new double[] {1, 1, 1});
+
+			Vector x = A.Solve(b);
+			Debug.Log("x = A^-1b: " + x);
+		}
+
+
+
+	public GameObject origin;
+    public GameObject dotPrefab;
+    public int numberOfDots = 0;
+    public Vector3[] points;
+    public Vector3 originPosition;
+
+
+    // Use this for initialization
+    void Start()
+    {
+		s ();
+        numberOfDots = 0;
+        originPosition = origin.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+            Instantiate(dotPrefab, new Vector3(mousePoint.x, mousePoint.y, this.transform.position.z), Quaternion.identity);
+            numberOfDots += 1;
+            Vector3[] newPoints = new Vector3[numberOfDots];
+            int i = 0;
+            foreach (Vector3 point in points) { newPoints[i] = point; i += 1; }
+            newPoints[numberOfDots - 1] = mousePoint;
+            points = newPoints;
+            print(numberOfDots);
+            print(points);
+            points[numberOfDots] = mousePoint;
+        }
+
+    }
+}
